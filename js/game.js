f69ca6064b61caa694c3aa5bef306962c812bbac
gmanteca <gabriel@gabrielmanteca.net>
2015-12-09 10:50:42 +0100

    usable version

--- js/game.js
+++ js/game.js
@@ -6,10 +6,11 @@ $('document').ready(function(){
 
 GAME.targets = [];
 GAME.points = 0;
-
+GAME.speed = 1;
+GAME.over = false;
 
 GAME.init = function(){
-  GAME.poinst = 0;
+  GAME.points = 0;
   $('#points').text('0 puntos.');
   setTimeout(function(){
     GAME.updateGame();
@@ -20,18 +21,18 @@ GAME.init = function(){
 
 GAME.updateGame = function(){
   var height = $('body').height();
-  console.log(GAME.targets);
+  var width = $('body').width();
   GAME.targets.forEach(function(t){
-    console.log(t);
-    t.bottom +=1;
-    if(t.bottom >= height){
+    t.update();
+    if(t.out(height,width)){
       GAME.lose();
       GAME.removeTarget(t);
     }
-    t.rep.css('bottom',t.bottom+'px');
   });
   setTimeout(function(){
-    console.log('test');
+    if(GAME.over){
+      return;
+    }
     GAME.updateGame();
   },30);
 }
@@ -39,20 +40,27 @@ GAME.updateGame = function(){
 GAME.lose = function(){
   $('.live').last().remove();
   if($('.live').length === 0){
-    //LOSE
+    GAME.over = true;
+    $('#over').css('display','block');
   }
 }
 
 GAME.addBalloons = function(){
   GAME.createTarget();
   setTimeout(function(){
+    if(GAME.over){
+      return;
+    }
     GAME.addBalloons();
-  }, Math.floor(Math.random()*4+2)*1000);
+  }, Math.floor(Math.random()*3)*(1000-GAME.speed*10));
 }
 
 GAME.setPoints = function(points){
   GAME.points += points;
   $('#points').text(GAME.points + ' puntos.');
+  if(GAME.points % 50 == 0){
+    GAME.speed++;
+  }
 }
 
 GAME.createTarget = function(){
@@ -61,11 +69,18 @@ GAME.createTarget = function(){
   target.rep = $balloon;
   target.bottom = -100;
   target.left = Math.floor(Math.random()*930);
+  target.update = function(){
+    target.bottom +=GAME.speed;
+    $balloon.css('bottom',target.bottom+'px');
+  }
+  target.out = function(height,width){
+    return target.bottom >= height;
+  }
   $balloon.css('height','100px').css('width','70px');
   $balloon.css('bottom',target.bottom+'px');
   $balloon.css('left',target.left+'px');
   GAME.targets.push(target);
-  switch(Math.floor(Math.random()*4)){
+  switch(Math.floor(Math.random()*5)){
     case 0:
     $balloon.addClass('green-balloon').on('click.game', function(e){
       e.preventDefault();
@@ -94,6 +109,25 @@ GAME.createTarget = function(){
       GAME.removeTarget(target);
     });
     break;
+    case 4:
+    $balloon.addClass('ufo').on('click.game', function(e){
+      e.preventDefault();
+      GAME.setPoints(100);
+      GAME.removeTarget(target);
+    });
+    $balloon.css('height','50px').css('width','80px');
+    target.bottom = Math.floor(Math.random()*$('body').height()-50);
+    target.left = -80;
+    $balloon.css('bottom',target.bottom+'px').css('left',target.left+'px');
+    target.update = function(){
+      target.left +=GAME.speed;
+      $balloon.css('left',target.left+'px');
+    }
+    target.out = function(height,width){
+      return target.left >= width;
+    }
+    break;
+
   }
   $balloon.appendTo($('.container'));
 }
